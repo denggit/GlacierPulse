24fae5a833362105d01c18bf5cd1e5c9c3ed779d
Zijun Deng <zijundeng0@gmail.com>
2026-04-20 23:45:17 +0800

    init docs

---
+++ delete_table.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Author     : Zijun Deng
+@Date       : 2/27/26 9:05 PM
+@File       : delete_table.py
+@Description:
+"""
+import os
+import sys
+
+# 添加项目根目录到 Python 路径
+current_file = os.path.abspath(__file__)
+project_root = os.path.dirname(current_file)
+if project_root not in sys.path:
+    sys.path.insert(0, project_root)
+
+import sqlite3
+from src.utils.log import get_logger
+
+logger = get_logger(__name__)
+
+# 连接到你的本地数据库
+conn = sqlite3.connect('data/crypto_history.db')
+cursor = conn.cursor()
+
+try:
+    # 直接精准爆破 30m 的表
+    cursor.execute("DROP TABLE ETH_USDT_SWAP_30m")
+    conn.commit()
+    logger.info("✅ 成功删除 30m 的脏数据表！其他周期数据已保留。")
+except Exception as e:
+    logger.info(f"删除失败或表不存在: {e}")
+
+conn.close()
