8b6b7539c07c2b4f6890e6b9f8324677bfb47732
Zijun Deng <42121907+denggit@users.noreply.github.com>
2026-04-28 11:24:48 +0800

    debug

--- tests/test_phase1_live.py
+++ tests/test_phase1_live.py
@@ -12,6 +12,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import logging
 from dotenv import load_dotenv
 
+from src.data_feed.okx_stream import OKXTickStreamer
 from src.data_feed.okx_books_stream import OKXBooksStreamer
 from src.context.market_context import MarketContext
 from src.detectors.iceberg_detector import IcebergDetector
